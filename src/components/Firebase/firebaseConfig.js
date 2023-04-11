8b913b22db9a50d2534088e09fa89bb7e7dab855
AnthonySar <anthony.sarapata@gmail.com>
2023-04-11 16:57:31 +0200

    :wrench: Ajout de l'Auth firebase & redirection useNvagiate

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBE5_qo8oBvaXHcnLQFD3IT6egRbXAKsU4",
+  authDomain: "react-fun-85e1f.firebaseapp.com",
+  projectId: "react-fun-85e1f",
+  storageBucket: "react-fun-85e1f.appspot.com",
+  messagingSenderId: "161111490973",
+  appId: "1:161111490973:web:28faf7388b5cdc8a2daf74"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
