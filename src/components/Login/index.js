e8dbaee2a1b901749a01c3314214559b1cdd399a
AnthonySar <anthony.sarapata@gmail.com>
2023-04-11 17:27:33 +0200

    :wrench: Ajout de la connexion firebase & redirection

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../Firebase/firebaseConfig';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [btn, setBtn] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (email !== '' && password.length > 5) {
+      setBtn(true)
+    } else if (btn) {
+      setBtn(false)
+    }
+  }, [email, password, btn]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    signInWithEmailAndPassword(auth, email, password)
+    .then(user => {
+      console.log(user)
+      setEmail('');
+      setPassword('');
+      navigate('/welcome');
+    }).catch(error => {
+      setError(error);
+      setEmail('');
+      setPassword('');
+    })
+  }
+
+  const btnCo = btn ? <button>Connexion</button> : <button disabled>Connexion</button>;
+
+  // Gestion des erreurs
+  const errorMsg = error !== "" && <span>{error.message}</span>;
+
   return (
     <div className='signUpLoginBox'>
       <div className='slContainer'>
-        Login
+        <div className='formBoxLeftLogin'></div>
+        <div className='formBoxRight'>
+          <div className='formContent'>
+            { errorMsg }
+
+            <h2>Connexion</h2>
+
+            <form onSubmit={handleSubmit}>
+              <div className='inputBox'>
+                <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id='email' required autoComplete='off' />
+                <label htmlFor='email'>Email</label>
+              </div>
+
+              <div className='inputBox'>
+                <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" id='password' required autoComplete='off' />
+                <label htmlFor='password'>Mot de passe</label>
+              </div>
+
+              { btnCo }
+            </form>
+
+            <div className='linkContainer'>
+              <Link className='simpleLink' to="/signup">Pas encore inscrit ? Let's go !</Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -48,12 +48,11 @@ const Signup = () => {
   return (
     <div className='signUpLoginBox'>
       <div className='slContainer'>
-        <div className='formBoxLeftSignup'>
-
-        </div>
+        <div className='formBoxLeftSignup'></div>
         <div className='formBoxRight'>
           <div className='formContent'>
             { errorMsg }
+            
             <h2>Inscription</h2>
 
             <form onSubmit={handleSubmit}>
@@ -61,18 +60,22 @@ const Signup = () => {
                 <input onChange={handleChange} value={pseudo} type="text" id='pseudo' required autoComplete='off' />
                 <label htmlFor='pseudo'>Pseudo</label>
               </div>
+
               <div className='inputBox'>
                 <input onChange={handleChange} value={email} type="email" id='email' required autoComplete='off' />
                 <label htmlFor='email'>Email</label>
               </div>
+
               <div className='inputBox'>
                 <input onChange={handleChange} value={password} type="password" id='password' required autoComplete='off' />
                 <label htmlFor='password'>Mot de passe</label>
               </div>
+
               <div className='inputBox'>
                 <input onChange={handleChange} value={confirmPassword} type="password" id='confirmPassword' required autoComplete='off' />
                 <label htmlFor='password'>Confirmer le mot de passe</label>
               </div>
+
               { btn }
             </form>
 
